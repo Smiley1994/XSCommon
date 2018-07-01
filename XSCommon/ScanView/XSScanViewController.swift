43045270e6aedf92c59bf12d9526b9a89a02679a
songf <smileysun1994@gmail.com>
2018-07-02 01:11:41 +0800

    add ScanViewController

---
+++ XSCommon/ScanView/XSScanViewController.swift
@@ -0,0 +1,35 @@
+//
+//  XSScanViewController.swift
+//  XSCommon
+//
+//  Created by Good_Morning_ on 2018/7/2.
+//  Copyright © 2018年 GoodMorning. All rights reserved.
+//
+
+import UIKit
+
+class XSScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
