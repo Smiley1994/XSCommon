57e0e5fad1b66bf6a4c6ad0245980445e2cd1b59
SunXiaoSong <sunxiaosong@ann9.com>
2018-07-12 20:42:34 +0800

    update

---
+++ XSCommon/UIUtils/XSTestButton.swift
@@ -0,0 +1,22 @@
+//
+//  XSTestButton.swift
+//  XSCommon
+//
+//  Created by 晓松 on 2018/7/12.
+//  Copyright © 2018年 GoodMorning. All rights reserved.
+//
+
+import UIKit
+
+class XSTestButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
