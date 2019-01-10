b2f6addfbbb76941dc266d93e3624032592ecc57
SunXiaoSong <sunxiaosong@ann9.com>
2019-01-10 20:37:29 +0800

    update animation circle

---
+++ XSCommon/Constants/XSFitDevice.swift
@@ -0,0 +1,13 @@
+//
+//  XSFitDevice.swift
+//  XSCommon
+//
+//  Created by 晓松 on 2019/1/9.
+//  Copyright © 2019 GoodMorning. All rights reserved.
+//
+
+import UIKit
+
+class XSFitDevice: NSObject {
+
+}
---
+++ XSCommon/UIUtils/XSAnimationCircleView.swift
@@ -0,0 +1,21 @@
+//
+//  XSAnimationCircleView.swift
+//  XSCommon
+//
+//  Created by 晓松 on 2019/1/10.
+//  Copyright © 2019 GoodMorning. All rights reserved.
+//
+
+import UIKit
+
+class XSAnimationCircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
